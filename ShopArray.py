69d5cf10d593050e9c29f0c38d9f6f811e4c04ba
Cl3s <82654514+Cl3s@users.noreply.github.com>
2022-02-16 11:33:52 +0900

    Update ShopArray.py

--- ShopArray.py
+++ ShopArray.py
@@ -3,14 +3,14 @@
             self.writeVint(1) # Ammount
             self.writeScId(16, 1) # Brawler ID
             self.writeVint(0) # Skin ID
-            self.writeVint(0) # ShopType [0 = Offer, 2 = Skins 3 = Star Shop]
+            self.writeVint(0) # ShopType, 0 = Offer, 2 = Skin, 3 = Star Shop
             self.writeVint(9) # Cost
             self.writeVint(99999) # Timer
             self.writeVint(1) # 1 Unknown
             self.writeVint(100) # 100 Unknown
             self.writeBoolean(False) # is Offer Purchased?
             self.writeBoolean(False) # False Unknown
-            self.writeVint(0) # ShopDisplay [0 = Normal, 1 = Daily Deals]
+            self.writeVint(0) # ShopDisplay, 0 = Normal, 1 = Daily Deals
             self.writeBoolean(False) # False Unknown
             self.writeVint(0) # 0 Unknown
             self.writeInt(0) # 0 Unknown(Error)
@@ -19,5 +19,5 @@
             self.writeString() # Backgrounds
             self.writeVint(0) # 0 Unknown
             self.writeBoolean(False) # True =  BG Disappear
-            self.writeVint(0) # Extra Type | 1 = (X)Profit, 2 = Profit in Percents(%)
+            self.writeVint(0) # Extra Type, 1 - (X)Profit, 2 - Profit in Percents(%)
             self.writeVint(0) # % Extra Text
