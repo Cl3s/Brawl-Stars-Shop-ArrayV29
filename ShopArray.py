561170d92d677170a38a24528635802ec5590836
Cl3s <82654514+Cl3s@users.noreply.github.com>
2022-03-02 23:37:48 +0900

    Update ShopArray.py

--- ShopArray.py
+++ ShopArray.py
@@ -1,7 +1,7 @@
             self.writeVint(1) # 1 Unknown(Error)
             self.writeVint(0) # ItemID
             self.writeVint(1) # Ammount
-            self.writeScId(16, 1) # Brawler ID
+            self.writeScId(16, 0) # Brawler ID
             self.writeVint(0) # Skin ID
             self.writeVint(0) # ShopType, 0 = Offer, 2 = Skin, 3 = Star Shop
             self.writeVint(9) # Cost
@@ -14,10 +14,10 @@
             self.writeBoolean(False) # False Unknown
             self.writeVint(0) # 0 Unknown
             self.writeInt(0) # 0 Unknown(Error)
-            self.write_string_reference('Name') # OfferName
+            self.write_string_reference('offerName') # OfferName
             self.writeBoolean(False) # False Unknown
             self.writeString() # Backgrounds
             self.writeVint(0) # 0 Unknown
-            self.writeBoolean(False) # True =  BG Disappear
+            self.writeBoolean(False) # True = BG Disappear
             self.writeVint(0) # Extra Type, 1 - (X)Profit, 2 - Profit in Percents(%)
             self.writeVint(0) # % Extra Text
